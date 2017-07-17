dbe35eb639cfebaed974d70fa43ed958e3c91703
Kanishka Mohan Madhuni <kmmadhuni@gmail.com>
2017-07-18 01:22:24 +0530

    Test case for the 'loadFeed' async function is written

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -83,13 +83,29 @@ $(function() {
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
+    describe('Initial Entries', function() {
+        
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            })
+        });
+        
+        it('feed loaded successfully', function(done) {
+            var entry = $('.feed .entry')[0];
+            expect(entry).toBeGreaterThan("");
+            done();
+        });         
 
+    });
+
+    
     /* TODO: Write a new test suite named "New Feed Selection" */
 
         /* TODO: Write a test that ensures when a new feed is loaded
